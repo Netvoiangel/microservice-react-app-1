8e135a4d0edceced6251857584804a1508e62814
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2024-03-28 18:35:28 +0300

    moderation comments and events saver created.

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -20,14 +20,15 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pendind' });
 
     await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pendind'
         }
     });
 
@@ -36,8 +37,31 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Event recieved:', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        });
+    }
 
     res.send({});
 });
